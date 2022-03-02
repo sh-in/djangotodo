00e49e37cfffbfb23cbcb7d211a36cf3298426b2
Shin Suzuki <shin.shin.desktoppc@gmail.com>
2022-03-02 11:50:02 +0900

    update delete function

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,10 +1,11 @@
 from unicodedata import name
 from django.urls import path
-from .views import TodoappCreate, TodoappDetail, TodoappList, TodoappUpdate
+from .views import TodoappCreate, TodoappDelete, TodoappDetail, TodoappList, TodoappUpdate
 
 urlpatterns = [
     path("", TodoappList.as_view(), name="list"),
     path("detail/<int:pk>", TodoappDetail.as_view(), name="detail"),
     path("create/", TodoappCreate.as_view(), name="create"),
-    path("update/<int:pk>", TodoappUpdate.as_view(), name="update")
+    path("update/<int:pk>", TodoappUpdate.as_view(), name="update"),
+    path("delete/<int:pk>", TodoappDelete.as_view(), name="delete"),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,7 +1,8 @@
 from dataclasses import fields
+from operator import mod
 from pyexpat import model
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from django.urls import reverse_lazy
 
@@ -24,4 +25,9 @@ class TodoappCreate(CreateView):
 class TodoappUpdate(UpdateView):
     model = Todoapp
     fields = "__all__"
+    success_url = reverse_lazy("list")
+
+class TodoappDelete(DeleteView):
+    model = Todoapp
+    context_object_name = "task"
     success_url = reverse_lazy("list")
